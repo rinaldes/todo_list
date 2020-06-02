d4e28f45a9cf2bcb6df0be57af70a5d399c86099
Rinal <rinaldes.duma@gmail.com>
2020-06-02 18:17:45 +0700

    20060202 - Tambah dan hapus TodoList

--- src/index.js
+++ src/index.js
@@ -1,17 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
+import { Card, Feed, Input, Form, Icon, List } from 'semantic-ui-react'
+
+class Todolist extends React.Component {
+    state = {
+        tugas: ["Tugas 1", "Tugas 2", "Tugas 3"]
+    };
+
+    deleteTask = (index) => {
+        const newArr = [...this.state.tugas];
+        newArr.splice(index, 1);
+        this.setState({ tugas: newArr });
+    }
+
+    newTask = task => {
+        this.setState({ tugas: [...this.state.tugas, task] });
+    }
+
+    render() {
+        return (
+            <Card grid centered middle>
+                <Card.Content>
+                    <Header jumlahTugas={this.state.tugas.length} />
+                </Card.Content>
+                <Card.Content>
+                    <Feed>
+                        <TaskList tugas={this.state.tugas} onDelete={this.deleteTask} />
+                    </Feed>
+                </Card.Content>
+                <Card.Content>
+                    <AddTask onFormSubmit={this.newTask} />
+                </Card.Content>
+            </Card>
+        );
+    }
+}
+
+class AddTask extends React.Component {
+    state = { term: "" };
+
+    submitTask = (e) => {
+        e.preventDefault();
+        if (this.state.term === "") return;
+        this.props.onFormSubmit(this.state.term);
+        this.setState({ term: "" });
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.submitTask}>
+                <Input
+                    icon={<Icon name='plus' inverted circular link />}
+                    type="text"
+                    className="input"
+                    placeholder="Tambahkan Tugas"
+                    value={this.state.term}
+                    onChange={(e) => this.setState({ term: e.target.value })}
+                    fluid
+                />
+            </Form>
+        );
+    }
+}
+
+function Header(props) {
+    return (
+        <Card.Header>Sisa {props.jumlahTugas} Tugas</Card.Header>
+    )
+}
+
+function TaskList(props) {
+    const daftarTugas = props.tugas.map((todo, index) => {
+        return <Todo content={todo} key={index} id={index} onDelete={props.onDelete} />
+    });
+
+    return (
+        <Feed.Event>
+            <Feed.Content>
+                <label>{daftarTugas}</label>
+            </Feed.Content>
+        </Feed.Event>
+    );
+}
+
+function Todo(props) {
+    return (
+        <List>
+            <List.Item>
+                {props.content}
+                <Icon
+                    name="trash alternate outline"
+                    onClick={() => { props.onDelete(props.id) }}
+                />
+            </List.Item>
+        </List>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Todolist />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
